57662f0372bfdc46ff30f55ad9863776f194c2fe
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-25 03:07:51 +0100

    (feat) -> geat and parse all employee data grom Api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,20 +9,20 @@ import requests
 
 
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/users"
-        todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos"
-        allusers = requests.get(url).json()
-        all_json = {}
-        for user in allusers:
-            id = user['id']
-            todos = requests.get(todo_url.format(id)).json()
-            all_json[id] = []
-            for todo in todos:
-                new_data = {}
-                new_data.update(task=todo["title"],
-                                completed=todo["completed"],
-                                username=user["username"]
-                                )
-                all_json[id].append(new_data)
-        with open(f"todo_all_employees.json", 'w') as f:
-            json.dump(all_json, f)
+    url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    allusers = requests.get(url).json()
+    all_json = {}
+    for user in allusers:
+        id = user['id']
+        todos = requests.get(todo_url.format(id)).json()
+        all_json[id] = []
+        for todo in todos:
+            new_data = {}
+            new_data.update(task=todo["title"],
+                            completed=todo["completed"],
+                            username=user["username"]
+                            )
+            all_json[id].append(new_data)
+    with open(f"todo_all_employees.json", 'w') as f:
+        json.dump(all_json, f)
