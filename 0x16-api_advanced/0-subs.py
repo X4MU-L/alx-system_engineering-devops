d2f0e4dfb4bea56e2c198cb92407d3b69564dd2c
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-08-14 16:13:45 +0100

    (feat) -> read a subreddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+"""fetch the number of subscribers for a particular subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit) -> int:
+    """Returns the number of subscribers for a subreddit
+    Args:
+        subreddit(string) -> The fetch focus.
+    """
+    header = {
+        "User-agent": "PostmanRuntime/7.32.3"
+    }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    result = requests.get(url, allow_redirects=False, headers=header)
+    about = result.json().get('data', None)
+    if about is None:
+        return 0
+    subs = about.get('subscribers', None)
+    return subs
